2295c55b7d692b50b6ec78eff524f353bb02efa2
Ovalere <ovalere86@gmail.com>
2024-09-17 19:03:12 +0700

    сделала JS

--- hw-2.js
+++ hw-2.js
@@ -1,15 +1,18 @@
+// Задание 1
 let a = 10;
 alert(a)
-
 let b = 20;
 alert(b)
 
+// Задание 2
 let year = "год выпуска первого iPhone";
 alert(year)
 
+// Задание 3
 let author = "имя создателя языка JavaScript";
 alert(author)
 
+// Задание 4
 let c = 10;
 let d = 2;
 let e = c + d;
@@ -21,22 +24,38 @@ alert(g)
 let h = c / d;
 alert(h)
 
+// Задание 5
 let result = 2 ** 5;
 alert(result)
 
+// Задание 6
 let i = 9;
 let j = 2;
 let k = i % j;
 alert(k)
 
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
 let age = prompt("Сколько вам лет?");
 alert(age);
 
-// let user = {
-//     name: "Kate",
-//     years: 20
-// };
-// let isAdmin = false
-// alert(name);
-// alert(years);
-// alert(isAdmin);
+// Задание 9
+let user = {  
+    name: "Kate",  
+    age: 20,
+    isAdmin: false,
+    };  
+
+// Задание 10
+
+let user = prompt ("Ваше имя?");
+alert (Привет, ИМЯ!)
