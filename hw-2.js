b7ca18a85f0a36ec0577ddf8db5e2b7728d95241
Ovalere <ovalere86@gmail.com>
2024-09-17 13:08:49 +0700

    создала файл js и мобилку

---
+++ hw-2.js
@@ -0,0 +1,42 @@
+let a = 10;
+alert(a)
+
+let b = 20;
+alert(b)
+
+let year = "год выпуска первого iPhone";
+alert(year)
+
+let author = "имя создателя языка JavaScript";
+alert(author)
+
+let c = 10;
+let d = 2;
+let e = c + d;
+alert(e)
+let f = c - d;
+alert(f)
+let g = c * d;
+alert(g)
+let h = c / d;
+alert(h)
+
+let result = 2 ** 5;
+alert(result)
+
+let i = 9;
+let j = 2;
+let k = i % j;
+alert(k)
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// let user = {
+//     name: "Kate",
+//     years: 20
+// };
+// let isAdmin = false
+// alert(name);
+// alert(years);
+// alert(isAdmin);
