5305bc7e7d081a2acd31c9bdf87d0a1e6db233ad
Ovalere <ovalere86@gmail.com>
2024-10-10 20:52:29 +0700

    сделала задания с кодом

--- hw-7.js
+++ hw-7.js
@@ -77,6 +77,52 @@ console.log(random());
 // Задание 7
 // Создать функцию, которая принимает два целых числа и
 //  возвращает случайное число в этом диапазоне.
-function number(a,b) {
-    return 
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+console.log(getRandomInt(2,5));
+
+
+// Задание 8
+// Вывести в консоль текущую дату.
+
+let currentDate = new Date();
+console.log(currentDate.toLocaleDateString('ru-RU'));
+
+// Задание 9
+// Создать переменную currentDate, хранящую текущую дату.
+//  Вывести дату, которая наступит через 73 дня после текущей.
+// Переменная currentDate создана, дата через 73 дня вычислена и 
+// выведена с использованием методов getDate() и setDate()
+
+let currentDate = new Date();
+let futureDate = new Date();
+futureDate.setDate(currentDate.getDate() + 73);
+
+console.log("Дата через 73 дня: " + futureDate.toDateString());
+
+// Задание 10
+// Написать функцию, которая принимает дату и возвращает ее в формате:
+// Дата: [число] [месяц на русском] [год] — это [день недели на русском].
+// Время: [часы]:[минуты]:[секунды]
+// Дату и время можно получить из объекта Date
+
+function formatDate(date) {
+    const months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+    const days = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+    const year = date.getFullYear();
+    const month = months[date.getMonth()];
+    const day = date.getDay();
+    const dayOfWeek = days[date.getDay()];
+    const hours = date.getHours();
+    const minutes =date.getMinutes();
+    const seconds =date.getSeconds();
+
+    return `Дата: ${day} ${month} ${year} — это ${dayOfWeek}.
+    Время: ${hours}:${minutes}:${seconds}`;
+}
+
+const now = new Date();
+console.log(formatDate(now));
